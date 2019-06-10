261d20e77883f3172d67199cee662987632f6395
E.J. Sexton <ejsexton82@gmail.com>
2019-06-10 12:43:25 +0200

    Moved SES Forwarder configuration to config.js

--- handler.js
+++ handler.js
@@ -1,31 +1,12 @@
 'use strict';
 
 var LambdaForwarder = require('aws-lambda-ses-forwarder');
+var config = require('./config');
 
 module.exports.sesForwarder = function (event, context, callback) {
-  // Configure the S3 bucket and key prefix for stored raw emails, and the
-  // mapping of email addresses to forward from and to.
-  //
-  // Expected keys/values:
-  // - fromEmail: Forwarded emails will come from this verified address
-  // - emailBucket: S3 bucket name where SES stores emails.
-  // - emailKeyPrefix: S3 key name prefix where SES stores email. Include the
-  //   trailing slash.
-  // - forwardMapping: Object where the key is the email address from which to
-  //   forward and the value is an array of email addresses to which to send the
-  //   message.
-  var overrides = {
-    config: {
-      fromEmail: "no-reply@ejsexton.com",
-      subjectPrefix: "",
-      emailBucket: process.env.bucketName,
-      emailKeyPrefix: "emails/",
-      forwardMapping: {
-        "@ejsexton.com": [
-          "ejsexton82+ejsexton.com@gmail.com"
-        ]
-      }
-    }
-  };
+  var overrides = {};
+  overrides.config = {...config.default, ...config[process.env.stage]};
+  overrides.config.emailBucket = process.env.bucketName;
+  overrides.config.emailKeyPrefix = "emails/";
   LambdaForwarder.handler(event, context, callback, overrides);
 }
